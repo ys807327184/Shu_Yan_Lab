f016d3ab48cf27abec7be26bf6681692367cfd4a
ys807327184 <55426896+ys807327184@users.noreply.github.com>
2021-07-23 14:38:03 +1200

    Update db_recordstore.sql

--- RecordStoreInfoSystem/database/db_recordstore.sql
+++ RecordStoreInfoSystem/database/db_recordstore.sql
@@ -2,10 +2,10 @@ CREATE DATABASE db_RecordStore;
 USE db_RecordStore;
 
 -- -----------------------------
--- Table structure for 'company'
+-- Table structure for `company`
 -- -----------------------------
-DROP TABLE IF EXISTS `db_recordstore`.`company`;
-CREATE TABLE `db_recordstore`.`company` (
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE `company` (
 	`companyId` int(5) NOT NULL AUTO_INCREMENT,
     `name` varchar(30) NOT NULL,
     `emailAddress` varchar(30) DEFAULT NULL,
@@ -14,6 +14,23 @@ CREATE TABLE `db_recordstore`.`company` (
 );
 
 -- -------------------------------
--- Table structure for 'albumtype'
+-- Table structure for `albumtype`
 -- -------------------------------
-DROP TABLE IF EXISTS `db_recordstore`.`albumtype`;
+DROP TABLE IF EXISTS `albumtype`;
+CREATE TABLE `albumtype` (
+	`albumTypeId` int(5) NOT NULL AUTO_INCREMENT,
+    `description` varchar(30) NOT NULL,
+    PRIMARY KEY (`albumTypeId`)
+);
+
+-- ----------------------------
+-- Table structure for `vendor`
+-- ----------------------------
+DROP TABLE IF EXISTS `vendor`;
+CREATE TABLE `vendor` (
+	`vendorId` int(5) NOT NULL AUTO_INCREMENT,
+    `name` varchar(30) NOT NULL,
+    `emailAddress` varchar(30) DEFAULT NULL,
+    `phoneNumber` varchar(30) DEFAULT NULL,
+    PRIMARY KEY (`vendorId`)
+);
