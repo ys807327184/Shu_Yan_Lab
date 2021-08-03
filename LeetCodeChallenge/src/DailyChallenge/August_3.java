7d200ca2d2bf5465489901ecd16fb7987ddad847
ys807327184 <55426896+ys807327184@users.noreply.github.com>
2021-08-03 15:01:56 +1200

    Update August_3.java

--- LeetCodeChallenge/src/DailyChallenge/August_3.java
+++ LeetCodeChallenge/src/DailyChallenge/August_3.java
@@ -47,6 +47,7 @@ public class August_3 {
 	//Version 2
 	//Runtime beats 9.06%
 	//Memory usage beats 64.42%
+	/*
 	public int[] twoSum(int[] nums, int target) {
 		int[] result = new int[2];
 		for (int i = 0; i < nums.length; i++) {
@@ -59,5 +60,18 @@ public class August_3 {
 			}
 		}
 		return result;
+	}
+	*/
+	//Version 3
+	public int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					System.gc();
+					return new int[] {i, j};
+				}
+			}
+		}
+		return null;
 	}
 }
