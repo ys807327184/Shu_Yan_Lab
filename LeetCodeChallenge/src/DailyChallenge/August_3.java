c47027f360987181e8e374985f575c63d6ce4a40
ys807327184 <55426896+ys807327184@users.noreply.github.com>
2021-08-03 14:40:44 +1200

    Update August_3.java

--- LeetCodeChallenge/src/DailyChallenge/August_3.java
+++ LeetCodeChallenge/src/DailyChallenge/August_3.java
@@ -27,6 +27,7 @@ public class August_3 {
 	//Version 1 
 	//Runtime beats 64.85%
 	//Memory usage beats 18.7%
+	/*
 	public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -42,4 +43,21 @@ public class August_3 {
         }
         return result;
     }
+    */
+	//Version 2
+	//Runtime beats 9.06%
+	//Memory usage beats 64.42%
+	public int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target && i != j) {
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+		return result;
+	}
 }
