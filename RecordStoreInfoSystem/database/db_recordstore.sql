ca8d759aa98c68029eca098592cf9bc0b4d5d1ff
ys807327184 <55426896+ys807327184@users.noreply.github.com>
2021-08-15 19:44:49 +1200

    Update db_recordstore.sql

--- RecordStoreInfoSystem/database/db_recordstore.sql
+++ RecordStoreInfoSystem/database/db_recordstore.sql
@@ -191,6 +191,7 @@ CREATE TABLE `accounts` (
 	`accountId` int(5) NOT NULL AUTO_INCREMENT,
     `username` varchar(30) NOT NULL,
     `password` varchar(30) NOT NULL,
+    `accountType` varchar(30) NOT NULL,
     PRIMARY KEY (`accountId`)
 );
 
