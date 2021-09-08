681a8b8ad5f01f44cf8340d058566f2badbce873
ys807327184 <55426896+ys807327184@users.noreply.github.com>
2021-09-08 18:21:53 +1200

    Create Challenge_21.java

---
+++ LeetCodeChallenge/src/Challenges/Challenge_21.java
@@ -0,0 +1,28 @@
+package Challenges;
+/*
+Date: 8 September 2021
+Challenge: Merge Two Sorted Lists
+	Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+	Example 1:
+		Input: l1 = [1,2,4], l2 = [1,3,4]
+		Output: [1,1,2,3,4,4]
+	Example 2:
+		Input: l1 = [], l2 = []
+		Output: []
+	Example 3:
+		Input: l1 = [], l2 = [0]
+		Output: [0]
+ 	Constraints:
+		The number of nodes in both lists is in the range [0, 50].
+		-100 <= Node.val <= 100
+		Both l1 and l2 are sorted in non-decreasing order.
+Developed by: Shu Yan
+ */
+public class Challenge_21 {
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode curr = l1, prev = null, next = l1.next;
+		while (l2 != null) {
+			
+		}
+	}
+}
