e03daad91cbf016c83124d386d5febcbdddacb41
ys807327184 <55426896+ys807327184@users.noreply.github.com>
2021-09-14 17:15:19 +1200

    Update Challenge_02.java

--- LeetCodeChallenge/src/Challenges/Challenge_02.java
+++ LeetCodeChallenge/src/Challenges/Challenge_02.java
@@ -22,8 +22,20 @@ Developed by: Shu Yan
  */
 public class Challenge_02 {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode curr = l1;
-		boolean plusOne;
-		
+		ListNode result = new ListNode(0);
+		ListNode curr = result;
+		int carry = 0;
+		while (l1 != null || l2 != null) {
+			int x = (l1 != null) ? l1.val : 0;
+			int y = (l2 != null) ? l2.val : 0;
+			int sum = x + y + carry;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (l1 != null) l1 = l1.next;
+			if (l2 != null) l2 = l2.next;
+		}
+		if (carry > 0) curr.next = new ListNode(carry);
+		return result.next;
 	}
 }
